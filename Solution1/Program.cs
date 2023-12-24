6886f32ae5772c21d4b098c1db62093930b996ab
K <k.g99@mail.ru>
2023-12-24 14:09:38 +0300

    Добавьте файлы проекта.

---
+++ Solution1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Входной файл "MLStart_2023\Solution1\input1.txt"
+        var input = File.ReadLines("..\\..\\..\\input1.txt", Encoding.UTF8);
+
+        // Напишите код  решения задачи ниже
+        int summa = 0;
+        Dictionary<string, int> map = new Dictionary<string, int>()
+        {
+            { "A X", 4 },
+            { "A Y", 8 },
+            { "A Z", 3 },
+            { "B X", 1 },
+            { "B Y", 5 },
+            { "B Z", 9 },
+            { "C X", 7 },
+            { "C Y", 2 },
+            { "C Z", 6 }
+        };
+        foreach (string line in input)
+        {
+            summa += map[line];
+        }
+        Console.WriteLine(summa);
+
+}   }
+
---
+++ Solution2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+public class Program
+{
+	public static void Main()
+	{
+		// Входной файл "MLStart_2023\Solution2\input2.txt"
+		var input = File.ReadLines("..\\..\\..\\input2.txt", Encoding.UTF8);
+
+		// Напишите код  решения задачи ниже
+
+	}
+}
